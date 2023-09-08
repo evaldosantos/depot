640c60475126f6d4a03898ed82a064c2cb154049
Evaldo Santos <evaldojr2@gmail.com>
2023-09-08 10:32:17 -0300

    styling products page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create!(
+    title: 'Build Chatbot Interactions',
+    description:
+    %{
+        <p>
+            <em>Responsible, Intuitive Interfaces with Ruby<em>
+            The next step in the evolution of user interfaces is here.
+            Chatbots let you users interact with your service in their
+            own natural language. Use free and open source tools along 
+            with Ruby to build createive, useful, and uexpected interactions 
+            for users. Take advantage of the Lita framework's step-by-setp
+            implementation strategy to simplify bot development and testing.
+            From novices to experts, chatbots are an areea in which 
+            everyone cvan participate. Exercise your crativity by creating
+             chatbot skills for communicating, inofrmation, and fun.
+
+        </p>
+    },
+    image_url: 'dchat.jpg',
+    price: 20.0
+)
